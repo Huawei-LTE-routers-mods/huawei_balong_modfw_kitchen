5ba8b568964e9309243815930137f2b6a5a48cfc
ValdikSS <iam@valdikss.org.ru>
2020-03-30 14:31:47 +0300

    Fix TTL/HL even if TTL of incoming packet == 1. Also handle IPv6.

--- base/v7r11/root/system/etc/fix_ttl.sh
+++ base/v7r11/root/system/etc/fix_ttl.sh
@@ -31,10 +31,20 @@ then
     FASTIP_FORWARD_ADDR="$(awk '/ _fastip_forward/ {print "0x"$1;exit}' /proc/kallsyms)"
     if [[ "$FASTIP_FORWARD_ADDR" ]];
     then
-        FASTIP_FORWARD_PATCH_OFFSET_1=$(($FASTIP_FORWARD_ADDR + 0x7C0))
-        FASTIP_FORWARD_PATCH_OFFSET_2=$(($FASTIP_FORWARD_ADDR + 0x7F0))
-        FASTIP_FORWARD_PATCH_OFFSET_3=$(($FASTIP_FORWARD_ADDR + 0x930))
-        FASTIP_FORWARD_PATCH_OFFSET_4=$(($FASTIP_FORWARD_ADDR + 0x960))
+        # FASTIP_ACTION_SNAT
+        FASTIP_FORWARD_PATCH_OFFSET_1=$(($FASTIP_FORWARD_ADDR + 0x7C0)) # 'SUB R3, R2, #1' (ip->ttl - 1) in fastip_dev_csum
+        FASTIP_FORWARD_PATCH_OFFSET_2=$(($FASTIP_FORWARD_ADDR + 0x7F0)) # 'SUB R3, R3, #1' ip->ttl--
+        FASTIP_FORWARD_PATCH_OFFSET_6=$(($FASTIP_FORWARD_ADDR + 0x79C)) # 'CMP R2, #1' (if (FASTIP_NUM_1 < ip->ttl))
+
+        # FASTIP_ACTION_DNAT
+        FASTIP_FORWARD_PATCH_OFFSET_3=$(($FASTIP_FORWARD_ADDR + 0x930)) # 'SUB R3, R2, #1'
+        FASTIP_FORWARD_PATCH_OFFSET_4=$(($FASTIP_FORWARD_ADDR + 0x960)) # 'SUB R3, R3, #1'
+        FASTIP_FORWARD_PATCH_OFFSET_7=$(($FASTIP_FORWARD_ADDR + 0x90C)) # 'CMP R2, #1' (if (FASTIP_NUM_1 < ip->ttl))
+
+        # IPv6
+        FASTIP_FORWARD_PATCH_OFFSET_5=$(($FASTIP_FORWARD_ADDR + 0x530)) # 'SUBHI R2, R2, #1'
+        FASTIP_FORWARD_PATCH_OFFSET_8=$(($FASTIP_FORWARD_ADDR + 0x52C)) # 'CMP R2, #1' (if (FASTIP_NUM_1 < ip6->hop_limit))
+
         TTL_HEX="$(printf "%02x" "$TTL")"
         # change SUB R3, R3, #1 to MOV R3, #64 for CTF_ACTION_SNAT
         wr_m $FASTIP_FORWARD_PATCH_OFFSET_1 $TTL_HEX 30 A0 E3
@@ -43,6 +53,16 @@ then
         # NOTE: COMMENT IF CTF_ACTION_DNAT PATCH IS NOT DESIRED
         wr_m $FASTIP_FORWARD_PATCH_OFFSET_3 $TTL_HEX 30 A0 E3
         wr_m $FASTIP_FORWARD_PATCH_OFFSET_4 $TTL_HEX 30 A0 E3
+        # MOVHI R2, HL
+        wr_m $FASTIP_FORWARD_PATCH_OFFSET_5 $TTL_HEX 20 A0 83
+
+        # Perform TTL/HL mangling even if TTL==1
+        # NOTE: COMMENT IF THIS PATCH IS NOT DESIRED
+        # CMP R2, #0
+        wr_m $FASTIP_FORWARD_PATCH_OFFSET_6 00
+        wr_m $FASTIP_FORWARD_PATCH_OFFSET_7 00
+        wr_m $FASTIP_FORWARD_PATCH_OFFSET_8 00
+
         echo "FastIP patched"
     else
         echo "ERROR: can't find FastIP address."
